8bcce9b3ac7dab0c40e7dbadc9eb62a46a0a88a6
faishal-developer <foysal.professional@gmail.com>
2024-03-26 15:38:04 +0600

    first commit

--- first-class-function-decorator.py
+++ first-class-function-decorator.py
@@ -1,11 +1,14 @@
 def func(another):
+    print(another())
+
     def call_another():
         another()
     return call_another
 
+# decorator
 
+
+@func
 def lastOne():
     print("lastone")
-
-
-print(func(lastOne)())
+    return 500
